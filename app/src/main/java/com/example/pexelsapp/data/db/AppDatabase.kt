f2f19ff93998874e21c1566a3814fd62a13445cd
Margarita <127426347+MargaritaPeresypkina@users.noreply.github.com>
2025-12-17 00:48:50 +0300

    refactor(data): delete RetrofitInstance.kt and companion object in AppDatabase to add it in DI module

--- app/src/main/java/com/example/pexelsapp/data/db/AppDatabase.kt
+++ app/src/main/java/com/example/pexelsapp/data/db/AppDatabase.kt
@@ -1,8 +1,6 @@
 package com.example.pexelsapp.data.db
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.pexelsapp.data.db.dao.FeaturedCollectionDao
 import com.example.pexelsapp.data.db.dao.PhotoDao
@@ -19,21 +17,4 @@ import com.example.pexelsapp.data.db.entities.PhotoEntity
 abstract class AppDatabase: RoomDatabase() {
     abstract fun photoDao(): PhotoDao
     abstract fun featuredCollectionDao(): FeaturedCollectionDao
-
-    companion object{
-        @Volatile
-        private var INSTANCE: AppDatabase? = null
-
-        fun getDatabase(context: Context): AppDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context = context.applicationContext,
-                    klass = AppDatabase::class.java,
-                    name = "pexels_app_database"
-                ).build()
-                INSTANCE = instance
-                instance
-            }
-        }
-    }
 }
