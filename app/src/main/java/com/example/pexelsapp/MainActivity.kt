128f18d53b9f59d1bddd298d399cf9e64bb2047a
Margarita <127426347+MargaritaPeresypkina@users.noreply.github.com>
2025-12-17 13:37:50 +0300

    feat(presentation): add base implementation splash screen and implement an icon

--- app/src/main/java/com/example/pexelsapp/MainActivity.kt
+++ app/src/main/java/com/example/pexelsapp/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.pexelsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,17 +10,41 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import com.example.pexelsapp.ui.theme.PexelsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setupSplashScreen()
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             PexelsAppTheme {
+                Scaffold { padding ->
+                    Text(
+                        text = "Hello Pexels",
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(padding)
+                    )
+                }
             }
+        }
+    }
+
+    private fun setupSplashScreen() {
+        val splashScreen = installSplashScreen()
+        var keepSplashOn = true
+        splashScreen.setKeepOnScreenCondition { keepSplashOn }
+
+        lifecycleScope.launch {
+            delay(2000)
+            keepSplashOn = false
         }
     }
 }
@@ -30,5 +53,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     PexelsAppTheme {
+        Text("Hello Preview")
     }
 }
